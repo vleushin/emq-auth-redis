46dc09b3462a2fa9494f1990784141afb9950c78
vleushin <vleushin@gmail.com>
2018-01-19 20:13:24 +0200

    Enhanced emq_auth_redis.erl to check client_id to begin with "<username>_" to prevent client_id abuse. Password check adjustments

--- src/emq_auth_redis.erl
+++ src/emq_auth_redis.erl
@@ -29,42 +29,48 @@
 -record(state, {auth_cmd, super_cmd, hash_type}).
 
 init({AuthCmd, SuperCmd, HashType}) ->
-    {ok, #state{auth_cmd = AuthCmd, super_cmd = SuperCmd, hash_type = HashType}}.
+  {ok, #state{auth_cmd = AuthCmd, super_cmd = SuperCmd, hash_type = HashType}}.
 
 check(#mqtt_client{username = Username}, Password, _State)
-    when ?UNDEFINED(Username); ?UNDEFINED(Password) ->
-    {error, username_or_password_undefined};
+  when ?UNDEFINED(Username); ?UNDEFINED(Password) ->
+  {error, username_or_password_undefined};
 
-check(Client, Password, #state{auth_cmd  = AuthCmd,
-                               super_cmd = SuperCmd,
-                               hash_type = HashType}) ->
-    Result = case emq_auth_redis_cli:q(AuthCmd, Client) of
-                {ok, PassHash} when is_binary(PassHash) ->
-                    check_pass(PassHash, Password, HashType);  
-                {ok, [undefined|_]} ->
-                    ignore;
-                {ok, [PassHash]} ->
-                    check_pass(PassHash, Password, HashType);
-                {ok, [PassHash, Salt|_]} ->
-                    check_pass(PassHash, Salt, Password, HashType);
-                {error, Reason} ->
-                    {error, Reason}
-             end,
-    case Result of ok -> {ok, is_superuser(SuperCmd, Client)}; Error -> Error end.
+check(Client, Password, #state{auth_cmd = AuthCmd,
+  super_cmd = SuperCmd,
+  hash_type = HashType}) ->
+  Result = case check_client_id(Client) of
+             ok -> case emq_auth_redis_cli:q(AuthCmd, Client) of
+                     {ok, PassHash} when is_binary(PassHash) ->
+                       check_pass(PassHash, Password, HashType);
+                     {ok, [undefined | _]} ->
+                       ignore;
+                     {ok, [PassHash]} ->
+                       check_pass(PassHash, Password, HashType);
+                     {ok, [PassHash, Salt | _]} ->
+                       check_pass(PassHash, Salt, Password, HashType);
+                     {error, Reason} ->
+                       {error, Reason}
+                   end;
+             _Error -> _Error
+           end,
+  case Result of
+    ok -> {ok, is_superuser(SuperCmd, Client)};
+    Error -> Error
+  end.
 
 check_pass(PassHash, Password, HashType) ->
-    check_pass(PassHash, hash(HashType, Password)).
+  check_pass(PassHash, hash(HashType, Password)).
 check_pass(PassHash, Salt, Password, {pbkdf2, Macfun, Iterations, Dklen}) ->
   check_pass(PassHash, hash(pbkdf2, {Salt, Password, Macfun, Iterations, Dklen}));
 check_pass(PassHash, Salt, Password, {salt, bcrypt}) ->
-    check_pass(PassHash, hash(bcrypt, {Salt, Password}));
+  check_pass(PassHash, hash(bcrypt, {Salt, Password}));
 check_pass(PassHash, Salt, Password, {salt, HashType}) ->
-    check_pass(PassHash, hash(HashType, <<Salt/binary, Password/binary>>));
+  check_pass(PassHash, hash(HashType, <<Salt/binary, Password/binary>>));
 check_pass(PassHash, Salt, Password, {HashType, salt}) ->
-    check_pass(PassHash, hash(HashType, <<Password/binary, Salt/binary>>)).
+  check_pass(PassHash, hash(HashType, <<Password/binary, Salt/binary>>)).
 
 check_pass(PassHash, PassHash) -> ok;
-check_pass(_, _)               -> {error, password_error}.
+check_pass(_, _) -> {error, password_error}.
 
 description() -> "Authentication with Redis".
 
@@ -72,12 +78,31 @@ hash(Type, Password) -> emqttd_auth_mod:passwd_hash(Type, Password).
 
 -spec(is_superuser(undefined | list(), mqtt_client()) -> boolean()).
 is_superuser(undefined, _Client) ->
-    false;
+  false;
+is_superuser("undefined", _Client) ->
+  false;
+is_superuser(<<"undefined">>, _Client) ->
+  false;
 is_superuser(SuperCmd, Client) ->
-    case emq_auth_redis_cli:q(SuperCmd, Client) of
-        {ok, undefined} -> false;
-        {ok, <<"1">>}   -> true;
-        {ok, _Other}    -> false;
-        {error, _Error} -> false
-    end.
+  case emq_auth_redis_cli:q(SuperCmd, Client) of
+    {ok, undefined} -> false;
+    {ok, <<"1">>} -> true;
+    {ok, _Other} -> false;
+    {error, _Error} -> false
+  end.
 
+
+% We expect ClientId to be in format username_someid to prevent ClientId abuse
+check_client_id(#mqtt_client{username = Username, client_id = ClientId}) ->
+  case byte_size(ClientId) =< byte_size(Username) + 1 of
+    true ->
+      {error, "Bad client id"};
+    false ->
+      ExpectedScope = {0, byte_size(Username) + 1},
+      case binary:match(ClientId, <<Username/binary, "_">>, [{scope, ExpectedScope}]) of
+        ExpectedScope ->
+          ok;
+        _Rest ->
+          {error, "Bad client id"}
+      end
+  end.
